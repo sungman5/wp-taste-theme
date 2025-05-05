7ff2ba9b78606b4d9b4b1b937935998f3661ef2b
이성만 <sungman5@iseongman-ui-Mac-Studio.local>
2025-05-06 00:57:33 +0900

    upload images

--- js/give-taste.js
+++ js/give-taste.js
@@ -4,55 +4,86 @@
 
 
     sectionDatas = [
-            {
-                index: 0,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'static',
-            },
-            {
-                index: 1,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 2,
-                heightNum: 3,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 3,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 4,
-                heightNum: 4,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 5,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 6,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-            {
-                index: 7,
-                heightNum: 1,
-                innerHeight: 0,
-                type: 'sticky',
-            },
-        ]
+        {
+            index: 0,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'static',
+        },
+        {
+            index: 1,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 2,
+            heightNum: 3,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 3,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 4,
+            heightNum: 4,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 5,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 6,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+        {
+            index: 7,
+            heightNum: 1,
+            innerHeight: 0,
+            type: 'sticky',
+        },
+    ]
+
+    // 플로팅 배너
+    function showDonationBanner() {
+        const web = document.getElementById('gt-floating-donation-web');
+        const scrollY = window.scrollY;
+        const triggerSection = document.getElementById('gt-1')
+        const mobile = document.getElementById('gt-floating-donation-mobile');
+
+        if (scrollY > triggerSection.offsetTop) {
+            if (window.innerWidth > 1440) {
+                web.style.opacity = 1;
+                web.style.visibility = 'visible';
+                mobile.style.opacity = 0;
+                mobile.style.visibility = 'hidden';
+            } else {
+                mobile.style.opacity = 1;
+                mobile.style.visibility = 'visible';
+                web.style.opacity = 0;
+                web.style.visibility = 'hidden';
+            }
+        } else {
+            web.style.opacity = 0;
+            web.style.visibility = 'hidden';
+            mobile.style.opacity = 0;
+            mobile.style.visibility = 'hidden';
+        }
+    }
+
+    window.addEventListener('scroll', showDonationBanner)
+    window.addEventListener('resize', showDonationBanner)
+
 
     const giveTasteEl = document.getElementById('give-taste');
     const baseUrl = giveTasteEl.dataset.url;
@@ -68,8 +99,12 @@
     ];
 
     document.addEventListener('click', (e) => {
+        const tagName = e.target.tagName.toLowerCase();
+        if (tagName === 'a' || tagName === 'button' || tagName === 'img' || tagName === 'input' || tagName === 'textarea' || e.target.isContentEditable) return;
+
         const centerX = e.clientX;
         const centerY = e.clientY;
+
 
         fruitImages.forEach((src, index) => {
             const fruit = document.createElement('img');
@@ -167,46 +202,46 @@
     function gt_2_animation() {
         const scrollY = window.scrollY;
         const gt_2_messages = document.querySelectorAll('#gt-2 .gt-message')
-        if (scrollY > sections[2].offsetTop + window.innerHeight * 0.5) {
+        if (scrollY > sections[2].offsetTop + window.innerHeight * 0.2) {
             gt_2_messages[0].style.opacity = 0;
             gt_2_messages[0].style.visibility = 'hidden';
-            gt_2_messages[0].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[2].offsetTop - window.innerHeight * 0.5) {
+            gt_2_messages[0].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[2].offsetTop - window.innerHeight * 0.2) {
             gt_2_messages[0].style.opacity = 1;
             gt_2_messages[0].style.visibility = 'visible';
             gt_2_messages[0].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_2_messages[0].style.opacity = 0;
             gt_2_messages[0].style.visibility = 'hidden';
-            gt_2_messages[0].style.transform = 'translate(-50%, -20%)';
+            gt_2_messages[0].style.transform = 'translate(-50%, -40%)';
         }
 
-        if (scrollY > sections[2].offsetTop + window.innerHeight * 1.5) {
+        if (scrollY > sections[2].offsetTop + window.innerHeight * 1.2) {
             gt_2_messages[1].style.opacity = 0;
             gt_2_messages[1].style.visibility = 'hidden';
-            gt_2_messages[1].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[2].offsetTop + window.innerHeight * 0.5) {
+            gt_2_messages[1].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[2].offsetTop + window.innerHeight * 0.2) {
             gt_2_messages[1].style.opacity = 1;
             gt_2_messages[1].style.visibility = 'visible';
             gt_2_messages[1].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_2_messages[1].style.opacity = 0;
             gt_2_messages[1].style.visibility = 'hidden';
-            gt_2_messages[1].style.transform = 'translate(-50%, -20%)';
+            gt_2_messages[1].style.transform = 'translate(-50%, -40%)';
         }
 
-        if (scrollY > sections[2].offsetTop + window.innerHeight * 2.5) {
+        if (scrollY > sections[2].offsetTop + window.innerHeight * 2.2) {
             gt_2_messages[2].style.opacity = 0;
             gt_2_messages[2].style.visibility = 'hidden';
-            gt_2_messages[2].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[2].offsetTop + window.innerHeight * 1.5) {
+            gt_2_messages[2].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[2].offsetTop + window.innerHeight * 1.2) {
             gt_2_messages[2].style.opacity = 1;
             gt_2_messages[2].style.visibility = 'visible';
             gt_2_messages[2].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_2_messages[2].style.opacity = 0;
             gt_2_messages[2].style.visibility = 'hidden';
-            gt_2_messages[2].style.transform = 'translate(-50%, -20%)';
+            gt_2_messages[2].style.transform = 'translate(-50%, -40%)';
         }
 
 
@@ -218,67 +253,185 @@
     function gt_4_animation() {
         const scrollY = window.scrollY;
         const gt_4_messages = document.querySelectorAll('#gt-4 .gt-message')
-        if (scrollY > sections[4].offsetTop + window.innerHeight * 0.5) {
+        if (scrollY > sections[4].offsetTop + window.innerHeight * 0.2) {
             gt_4_messages[0].style.opacity = 0;
             gt_4_messages[0].style.visibility = 'hidden';
-            gt_4_messages[0].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[4].offsetTop - window.innerHeight * 0.5) {
+            gt_4_messages[0].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[4].offsetTop - window.innerHeight * 0.2) {
             gt_4_messages[0].style.opacity = 1;
             gt_4_messages[0].style.visibility = 'visible';
             gt_4_messages[0].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_4_messages[0].style.opacity = 0;
             gt_4_messages[0].style.visibility = 'hidden';
-            gt_4_messages[0].style.transform = 'translate(-50%, -20%)';
+            gt_4_messages[0].style.transform = 'translate(-50%, -40%)';
         }
 
-        if (scrollY > sections[4].offsetTop + window.innerHeight * 1.5) {
+        if (scrollY > sections[4].offsetTop + window.innerHeight * 1.2) {
             gt_4_messages[1].style.opacity = 0;
             gt_4_messages[1].style.visibility = 'hidden';
-            gt_4_messages[1].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 0.5) {
+            gt_4_messages[1].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 0.2) {
             gt_4_messages[1].style.opacity = 1;
             gt_4_messages[1].style.visibility = 'visible';
             gt_4_messages[1].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_4_messages[1].style.opacity = 0;
             gt_4_messages[1].style.visibility = 'hidden';
-            gt_4_messages[1].style.transform = 'translate(-50%, -20%)';
+            gt_4_messages[1].style.transform = 'translate(-50%, -40%)';
         }
 
-        if (scrollY > sections[4].offsetTop + window.innerHeight * 2.5) {
+        if (scrollY > sections[4].offsetTop + window.innerHeight * 2.2) {
             gt_4_messages[2].style.opacity = 0;
             gt_4_messages[2].style.visibility = 'hidden';
-            gt_4_messages[2].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 1.5) {
+            gt_4_messages[2].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 1.2) {
             gt_4_messages[2].style.opacity = 1;
             gt_4_messages[2].style.visibility = 'visible';
             gt_4_messages[2].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_4_messages[2].style.opacity = 0;
             gt_4_messages[2].style.visibility = 'hidden';
-            gt_4_messages[2].style.transform = 'translate(-50%, -20%)';
+            gt_4_messages[2].style.transform = 'translate(-50%, -40%)';
         }
 
-        if (scrollY > sections[4].offsetTop + window.innerHeight * 3.5) {
+        if (scrollY > sections[4].offsetTop + window.innerHeight * 3.2) {
             gt_4_messages[3].style.opacity = 0;
             gt_4_messages[3].style.visibility = 'hidden';
-            gt_4_messages[3].style.transform = 'translate(-50%, -80%)';
-        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 2.5) {
+            gt_4_messages[3].style.transform = 'translate(-50%, -70%)';
+        } else if (scrollY > sections[4].offsetTop + window.innerHeight * 2.2) {
             gt_4_messages[3].style.opacity = 1;
             gt_4_messages[3].style.visibility = 'visible';
             gt_4_messages[3].style.transform = 'translate(-50%, -50%)';
         } else {
             gt_4_messages[3].style.opacity = 0;
             gt_4_messages[3].style.visibility = 'hidden';
-            gt_4_messages[3].style.transform = 'translate(-50%, -20%)';
+            gt_4_messages[3].style.transform = 'translate(-50%, -40%)';
         }
     }
 
 
     window.addEventListener('scroll', gt_4_animation);
 
+    const stickers = document.querySelectorAll('.gt-sticker-btn')
+    const checkedMark = document.querySelectorAll('.gt-checked-mark')
+    let checkedSticker = [];
+    let username;
+    console.log(stickers)
 
+    const juiceMap = {
+        'gt-cheer,gt-duty,gt-happy': `juice-1.jpg`,
+        'gt-cheer,gt-duty,gt-thanks': 'juice-2.jpg',
+        'gt-cheer,gt-duty,gt-hope': 'juice-3.jpg',
+        'gt-cheer,gt-compassion,gt-duty': 'juice-4.jpg',
+        'gt-duty,gt-happy,gt-thanks': 'juice-5.jpg',
+        'gt-duty,gt-happy,gt-hope': 'juice-6.jpg',
+        'gt-compassion,gt-duty,gt-happy': 'juice-7.jpg',
+        'gt-duty,gt-hope,gt-thanks': 'juice-8.jpg',
+        'gt-compassion,gt-duty,gt-thanks': 'juice-9.jpg',
+        'gt-compassion,gt-duty,gt-hope': 'juice-10.jpg',
+        'gt-cheer,gt-happy,gt-thanks': 'juice-11.jpg',
+        'gt-cheer,gt-happy,gt-hope': 'juice-12.jpg',
+        'gt-cheer,gt-compassion,gt-happy': 'juice-13.jpg',
+        'gt-cheer,gt-hope,gt-thanks': 'juice-14.jpg',
+        'gt-cheer,gt-compassion,gt-thanks': 'juice-15.jpg',
+        'gt-cheer,gt-compassion,gt-hope': 'juice-16.jpg',
+        'gt-happy,gt-hope,gt-thanks': 'juice-17.jpg',
+        'gt-compassion,gt-happy,gt-thanks': 'juice-18.jpg',
+        'gt-compassion,gt-happy,gt-hope': 'juice-19.jpg',
+        'gt-compassion,gt-hope,gt-thanks': 'juice-20.jpg'
+    };
 
+    const makeBtn = document.getElementById('gt-make-button');
+    const nameInput = document.getElementById('gt-username');
+    const juiceFrame = document.getElementById('gt-show-result');
+    const juiceImage = document.getElementById('gt-juice-img');
+
+    function makeJuice() {
+        makeBtn.addEventListener('click', () => {
+            username = nameInput.value;
+            const sortedCombo = checkedSticker.slice().sort().join(',');
+
+            if (checkedSticker.length === 3) {
+                const result = juiceMap[sortedCombo];
+                if (result) {
+                    juiceImage.src = `${baseUrl}/img/give-taste/${result}`;
+                    juiceFrame.style.opacity = 1;
+                    juiceFrame.style.visibility = 'visible';
+                } else {
+                    console.log('알 수 없는 조합입니다.');
+                }
+            } else {
+                alert('3가지 맛을 선택해주세요!');
+            }
+        });
+    }
+
+    makeJuice();
+
+    function closeJuice() {
+        const closeBtn = document.getElementById('gt-show-btn-close')
+        closeBtn.addEventListener('click', () => {
+            juiceFrame.style.opacity = 0;
+            juiceFrame.style.visibility = 'hidden';
+        })
+    }
+
+    closeJuice();
+
+    function selectedSticker() {
+        for (let i = 0; i < stickers.length; i++) {
+            stickers[i].addEventListener('click', (e) => {
+                // Get the associated input's ID from the label's 'for' attribute
+                const id = stickers[i].getAttribute('for');
+                const index = checkedSticker.indexOf(id);
+                if (index > -1) {
+                    // 이미 선택된 경우 → 선택 해제
+                    checkedSticker.splice(index, 1);
+                    checkedMark[i].style.opacity = 0;
+                    checkedMark[i].style.visibility = 'hidden';
+                    console.log('removed:', id, checkedSticker);
+                } else {
+                    if (checkedSticker.length < 3) {
+                        checkedMark[i].style.opacity = 1;
+                        checkedMark[i].style.visibility = 'visible';
+                        checkedSticker.push(id);
+                        console.log('added:', id, checkedSticker);
+                    } else {
+                        console.log('nope');
+                    }
+                }
+            })
+        }
+    }
+
+    selectedSticker();
+
+    const btn = document.querySelector('.gt-go-to-top');
+
+    function goToTop() {
+        btn.addEventListener('click', () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth' // 부드럽게 스크롤
+            });
+        })
+    }
+
+    goToTop();
+
+    function showGoToTopBtn(){
+        const scrollY = window.scrollY;
+        const triggerSection = document.getElementById('gt-6')
+        if(scrollY > triggerSection.offsetTop){
+            btn.style.opacity = 1;
+            btn.style.visibility = 'visible';
+        } else{
+            btn.style.opacity = 0;
+            btn.style.visibility = 'hidden';
+        }
+    }
+
+    window.addEventListener('scroll', showGoToTopBtn)
 
 })();
--- page-give-taste.php
+++ page-give-taste.php
@@ -20,8 +20,19 @@
 
 <div id="give-taste" data-url="<?php echo get_template_directory_uri() ?>">
     <canvas id="bubble-canvas"></canvas>
+    <a id="gt-floating-donation-web">
+        <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/gt-btn-donate-fix.svg" alt="">
+    </a>
+    <div class="gt-go-to-top">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
+            <path d="M205.66,138.34a8,8,0,0,1-11.32,11.32L136,91.31V224a8,8,0,0,1-16,0V91.31L61.66,149.66a8,8,0,0,1-11.32-11.32l72-72a8,8,0,0,1,11.32,0ZM216,32H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path>
+        </svg>
+        <p>처음으로</p>
+    </div>
     <div class="gt-header">
-        <p>기부문화캠페인</p>
+        <img id="gt-bf-logo" src="<?php echo get_template_directory_uri() ?>/img/give-taste/gt-bf-logo.svg" alt="아름다운재단 로고">
+        <img id="gt-bf-logo-mobile" src="<?php echo get_template_directory_uri() ?>/img/give-taste/gt-bf-logo-mobile.svg" alt="아름다운재단 모바일 로고">
+        <p>아름다운재단 기부문화캠페인</p>
     </div>
     <section class="gt-section" id="gt-0">
         <p class="gt-title">
@@ -46,7 +57,7 @@
             <p class="gt-message">우리는 어떤 마음을 담아 기부합니다.</p>
         </div>
         <div class="gt-msg-background">
-            <p class="gt-message">하지만 마음은 눈에 보이지 않아요.</p>
+            <p class="gt-message">하지만 마음은 <br class="gt-only-mobile-show">눈에 보이지 않아요.</p>
         </div>
         <div class="gt-msg-background">
             <p class="gt-message">
@@ -61,7 +72,7 @@
     <section class="gt-section" id="gt-4">
         <div class="gt-msg-background">
             <p class="gt-message">
-                우리는 기부 그리고 나누는 마음에 대해
+                우리는 기부 그리고 나누는 마음에 대해<br>
                 조금 더 편안하고 즐겁게 이야기할 수 있으면 좋겠습니다.
             </p>
         </div>
@@ -76,54 +87,143 @@
             </p>
         </div>
         <div class="gt-msg-background">
-            <p class="gt-message">당신의 기부는, 어떤 맛인가요?</p>
+            <p class="gt-message">당신의 기부는,<br class="gt-only-mobile-show"> 어떤 맛인가요?</p>
         </div>
     </section>
     <section class="gt-section" id="gt-5">
-        <ul class="gt-check-sticker">
-            <li>
-                <label class="gt-sticker-btn" for="gt-hope">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_1.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-hope">
-            </li>
-            <li>
-                <label class="gt-sticker-btn" for="gt-thanks">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_2.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-thanks">
-            </li>
-            <li>
-                <label class="gt-sticker-btn" for="gt-cheer">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_3.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-cheer">
-            </li>
-            <li>
-                <label class="gt-sticker-btn" for="gt-responsibility">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_4.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-responsibility">
-            </li>
-            <li>
-                <label class="gt-sticker-btn" for="gt-compassion">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_5.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-compassion">
-            </li>
-            <li>
-                <label class="gt-sticker-btn" for="gt-happy">
-                    <img src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_6.png" alt="">
-                </label>
-                <input class="gt-checkbox" type="checkbox" id="gt-happy">
-            </li>
-        </ul>
+        <div class="gt-check-sticker">
+            <p>
+                3가지 마음을 선택해<br>
+                나만의 기부주스를 만들어보세요.
+            </p>
+            <ul>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-hope">
+                        <img id="gt-sticker-hope" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_1.png" alt="희망 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-hope">
+                </li>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-thanks">
+                        <img id="gt-sticker-thanks" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_2.png" alt="감사 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-thanks">
+                </li>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-cheer">
+                        <img id="gt-sticker-cheer" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_3.png" alt="응원 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-cheer">
+                </li>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-duty">
+                        <img id="gt-sticker-duty" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_4.png"
+                             alt="책임감 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-duty">
+                </li>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-compassion">
+                        <img id="gt-sticker-compassion" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_5.png"
+                             alt="동정심 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-compassion">
+                </li>
+                <li>
+                    <label class="gt-sticker-btn" for="gt-happy">
+                        <img id="gt-sticker-happy" class="gt-sticker"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/sticker_6.png" alt="행복 스티커">
+                        <img class="gt-checked-mark"
+                             src="<?php echo get_template_directory_uri() ?>/img/give-taste/check-fat-fill.svg"
+                             alt="체크 스티커">
+                    </label>
+                    <input class="gt-checkbox" type="checkbox" id="gt-happy">
+                </li>
+            </ul>
+            <div class="gt-make-juice">
+                <input id="gt-username" type="text" placeholder="이름을 입력하세요">
+                <button id="gt-make-button">주스 만들기</button>
+            </div>
+        </div>
+        <div id="gt-show-result">
+            <div class="gt-show-juice">
+                <img id="gt-juice-img" src="<?php echo get_template_directory_uri() ?>/img/give-taste/juice-6.jpg"
+                     alt="">
+                <div class="gt-show-options">
+                    <button id="gt-show-btn-save">
+                        저장
+                    </button>
+                    <button id="gt-show-btn-close">
+                        닫기
+                    </button>
+                </div>
+
+            </div>
+        </div>
     </section>
     <section class="gt-section" id="gt-6">
-        기부하기 버튼
+        <p class="gt-6-headline">
+            기부에 대한 이야기들이 <br class="gt-only-mobile-show">자연스러워지고 더 다양해지길 바랍니다.<br>
+            아름다운재단은 건강한 기부문화를 만들어가겠다는<br>
+            첫 약속을 지켜가겠습니다.
+        </p>
+        <div class="gt-donation-message">
+            <p>
+                지원을 받는 사람도, 기부자도, 모금 기관도<br>
+                건강한 기부문화 속에서 지속적이고 긍정적인<br>
+                사회변화를 경험할 수 있습니다.
+            </p>
+            <p>
+                그래서 아름다운 재단은 이 노력을 지속하고 싶습니다.
+            </p>
+            <p>
+                당신의 기부가 더 건강하게<br>
+                우리 사회에 자리잡을 수 있도록<br>
+                아름다운재단과 함께해주세요.
+            </p>
+        </div>
+        <a href="">기부하기</a>
+        <p class="gt-6-caption">
+            기부금은 1%다달나눔기금을 통해 사각지대를 발굴하고, <br class="gt-only-mobile-show">변화를 만드는 아름다운재단의 다양한 활동에 지원됩니다.
+        </p>
     </section>
     <section class="gt-section" id="gt-7">
-        외부링크
+        <div class="gt-outlinks">
+            <a href="#">
+                아름다운재단에는<br>
+                우는 아이가 없습니다.
+            </a>
+            <a href="#">
+                별나라로 간<br>
+                건강한 기부
+            </a>
+            <a href="#">
+                기부 연결 지도
+            </a>
+        </div>
     </section>
+    <a id="gt-floating-donation-mobile">
+        캠페인 기부하기
+    </a>
 </div>
 </html>
